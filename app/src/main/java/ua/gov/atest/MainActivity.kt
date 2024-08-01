c6c4626c631b6e0b42a2625e118c33d2b6c2b0df
Николаєнко Олександр Юрійович <olexander.nikolaenko@amedia.ua>
2024-08-01 10:59:32 +0300

    fix 1

--- app/src/main/java/ua/gov/atest/MainActivity.kt
+++ app/src/main/java/ua/gov/atest/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     //lol
     //lol
+    //fix
     Text(
         text = "Hello $name!",
         modifier = modifier
